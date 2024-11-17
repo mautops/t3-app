6fca715bbfc368f765f96137ae5ae0a94d0439c7
peizhenfei <peizhenfei@cvte.com>
2024-11-17 21:55:10 +0800

    debug: nextauth.js token

--- src/config.ts
+++ src/config.ts
@@ -26,10 +26,11 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+  interface User {
+    accessToken: string;
+    // ...other properties
+    // role: UserRole;
+  }
 }
 
 /**
