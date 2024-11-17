8e2db922315b8c139d050bf450dbfe2f1e1a1e91
peizhenfei <peizhenfei@cvte.com>
2024-11-17 12:11:51 +0800

    debug: nextauth.js token

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-// import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
 
-// import { db } from "~/server/db";
-// import {
-//   accounts,
-//   sessions,
-//   users,
-//   verificationTokens,
-// } from "~/server/db/schema";
+import { db } from "~/server/db";
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+} from "~/server/db/schema";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
